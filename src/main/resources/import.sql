66095dff9b0573f7f418ce1797fb3ca12e2dc54e
David Nguyen <hungson215@gmail.com>
2018-02-15 23:36:10 -0500

    Add import.sql Fix the search bar

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+/*Populate all the tables in here*/
+
+/*USER TABLE */
+INSERT INTO user(user_name,password,email,phone,account_type,date_created) VALUES ("admin","123","admin@gmail.com","800255600","admin","2018-1-2");
+INSERT INTO user(user_name,password,email,phone,account_type,date_created) VALUES ("client","123","client@gmail.com","4045006019","client","2018-1-3");
+INSERT INTO user(user_name,password,email,phone,account_type,date_created) VALUES ("doctor","123","doctor@gmail.com","6789331234","doctor","2018-1-4");
